62a39db38a41ef6b1b8abdd46879d3e7a3a140a8
G r e y <vapurrmaid@pm.me>
2021-06-18 01:47:56 -0500

    refactor: Simplify App state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { GameGrid } from "./components/GameGrid";
 import { Header } from "./components/Header";
 import { Layout } from "./components/Layout";
 import { Game, GameValues } from "./entities/game";
 
 export const App: React.FC = () => {
-  const [, setSelectedValue] = useState<GameValues>(Game.current);
-
   useEffect(() => {
-    const updateGameValue = () => setSelectedValue(Game.next());
+    const updateGameValue = () => Game.next();
     const interval = window.setInterval(updateGameValue, 2000);
     return () => window.clearInterval(interval);
   }, []);
